4b5b6d66c33d18e393a97425e3e9b2faf2d8c1ee
inaaka <91781115+inaaka@users.noreply.github.com>
2023-01-18 22:16:57 +0900

    Add files via upload

---
+++ FFTv1.0/fft_define.h
@@ -0,0 +1,17 @@
+/* macro definition for fft */
+
+#ifndef FFT_DEFINE_H
+#define FFT_DEFINE_H
+
+/* macro definition */
+#define fftPI2 6.283185307179586
+
+/* type definition */
+/* struct complex number */
+typedef struct fftcomplex_t
+{
+    double real;
+    double imag;
+} fftComplex;
+
+#endif
